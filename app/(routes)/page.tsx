910519586a93b4b28663f9e4b0c2f333db8d68a1
Varunjoshi22 <92745621+Varunjoshi22@users.noreply.github.com>
2024-09-26 11:25:59 +0530

    Product cards Feature Added

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,24 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true});
+    const billboard = await getBillboard("be18a71d-08ea-40a0-bae6-54604ab9657d")
     return ( 
         <div>
-            hellow store
+            <Container>
+                <div className="space-y-10 pb-10">
+                    <Billboard data={billboard}/>
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-6">
+                    <ProductList title="Featured Products" items={products}/>
+                </div>
+                </div>
+            </Container>
         </div>
      );
 }
