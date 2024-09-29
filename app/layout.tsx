c98c8c9910129818c3681fd2b533d0531220c23b
Varunjoshi22 <92745621+Varunjoshi22@users.noreply.github.com>
2024-09-29 21:08:54 +0530

    Product Preview Modal Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Urbanist} from 'next/font/google';
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import ModalProvider from "@/providers/modal-provider";
 
 
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body
         className={font.className}
       >
+        <ModalProvider/>
         <Navbar/>
         {children}
         <Footer/>
